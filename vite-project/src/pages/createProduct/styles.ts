92fe7e0e70ee04a9b01ccf274b9a7f1d0ecf16d5
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-10-27 20:48:16 -0300

    styles form

---
+++ vite-project/src/pages/createProduct/styles.ts
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const ContentDiv = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h2 {
+    margin: 20px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+  }
+
+  input {
+    border-radius: 10px;
+    color: black;
+    padding-left: 5px;
+    width: 100%;
+  }
+
+  section {
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    padding-top: 10px;
+  }
+  
+  button {
+    display: flex;
+    height: 25px;
+    width: 55px;
+    border-radius: 10px;
+    background-color: whitesmoke;
+    color: black;
+    border: none;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    :hover {
+      background-color: blue;
+    }
+  }
+`;
